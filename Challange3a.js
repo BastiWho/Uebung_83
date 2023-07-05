f5c4fe877b4d63e66e7fa95160bbba3b9e6c967c
Basti <sebastian.hufeld@docc.techstarter.de>
2023-07-05 17:09:01 +0200

    Challange 3a aus der Übung 83

---
+++ Challange3a.js
@@ -0,0 +1,5 @@
+const bubbleSort = require('./Challange2.js');
+
+const unsortedArray = [21, 3, 45213, 2342, 45, 10, 900];
+const sortedArray = bubbleSort(unsortedArray);
+console.log((sortedArray));
